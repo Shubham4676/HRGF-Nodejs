2ab7c6c8b9544f0db83771c00290387a880081af
Shubham4676 <meghraj4467@gmail.com>
2025-10-16 20:13:51 +0530

    EKS Files

---
+++ terraform_code/eks_code/outputs.tf
@@ -0,0 +1,15 @@
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "node_group_role_name" {
+  value = try(module.eks.eks_managed_node_groups["Nodejs_Node"].iam_role_name, null)
+}
--- terraform_code/eks_code/vpc.tf
+++ terraform_code/eks_code/vpc.tf
@@ -11,12 +11,20 @@ module "vpc" {
   intra_subnets   = local.intra_subnets
 
   enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
   }
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
   }
+
+  tags = local.tags
 }
